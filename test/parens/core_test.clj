d81b9abbbb1b648c8e5d05c8007b69042cc1397c
Hugh Rawlinson <hugh@spotify.com>
2017-01-15 02:18:16 +0100

    Edit code formatting
    In an attempt to stop myself from crying so hard

--- test/parens/core_test.clj
+++ test/parens/core_test.clj
@@ -2,32 +2,20 @@
   (:require [clojure.test :refer :all]
             [parens.core :refer :all]))
 
-;; (deftest a-test
-;;   (testing "FIXME, I fail."
-;;     (is (= 0 1))))
-
-;; (deftest filterParens-filters-a-character
-;;   (testing "Returns a string only containing parens"
-;;     (is (re-matches #"^[\(|\)]*$" (filterParens "(a)")))))
-
-;; (deftest filterParens-matches-parens-only-string
-;;   (testing "matches a parens only string"
-;;     (is (re-matches #"^[\(|\)]*$" (filterParens "()()())))(((()())))")))))
-
-;; (deftest filterParens-matches-empty-string
-;;   (testing "matches a parens only string"
-;;     (is (re-matches #"^[\(|\)]*$" (filterParens "")))))
-
 (deftest validateParenthesesTest
   (testing "Parenthesis nesting validation"
-    (is (and (every? true? (map validateParentheses [
-                                                ""
-                                                "()"
-                                                "((()())())"
-                                                     ]))
-             (every? false? (map validateParentheses [
-                                                    ")"
-                                                    ")("
-                                                    "())()"
-                                                    "()()())"
-                                                    ]))))))
+    (is (and (every? true?
+                     (map validateParentheses
+                          [
+                           ""
+                           "()"
+                           "((()())())"
+                           ]))
+             (every? false?
+                     (map validateParentheses
+                          [
+                           ")"
+                           ")("
+                           "())()"
+                           "()()())"
+                           ]))))))
